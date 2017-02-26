e58af48006045656b42e785051743c91d103f1b4
jonypx19 <jonathan.pelastine@mail.utoronto.ca>
2017-02-26 18:24:05 -0500

    Reworked signup activity to include password field

--- Campfire/app/src/main/java/com/example/jonat/campfire/SignUpActivity.java
+++ Campfire/app/src/main/java/com/example/jonat/campfire/SignUpActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SignUpActivity extends AppCompatActivity {
 
