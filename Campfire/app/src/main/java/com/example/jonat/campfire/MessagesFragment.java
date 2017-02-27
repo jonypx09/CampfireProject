f2aa352f2f850d25ebb0af8d2bae29e8035587fe
Andrew Goupil <andrew.goupil@mail.utoronto.ca>
2017-02-27 16:47:05 -0500

    Added super barebones messenger page, that you navigate to after clicking on a conversation

--- Campfire/app/src/main/java/com/example/jonat/campfire/MessagesFragment.java
+++ Campfire/app/src/main/java/com/example/jonat/campfire/MessagesFragment.java
@@ -2,14 +2,18 @@ package com.example.jonat.campfire;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ShareCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import static android.R.attr.fragment;
 
 /**
  * Created by jonat on 25-Feb-2017.
@@ -50,7 +54,9 @@ public class MessagesFragment extends Fragment{
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                // go to conversation
+                Intent intent = new Intent(getActivity(), MessengerActivity.class);
+                //intent.putExtra(); for when we are passing which messages to look at
+                startActivity(intent);
             }
         });
         //you can set the title for your toolbar here for different fragments different titles
