ddc5aefe8307de8c3c54f2daf29849a6cb4b0ce7
Quinn Daneyko <quinn.daneyko@mail.utoronto.ca>
2017-02-27 02:17:17 -0500

    First iteration (barebones) of 'My Campfire' is complete

--- Campfire/app/src/main/java/com/example/jonat/campfire/MyCampfireFragment.java
+++ Campfire/app/src/main/java/com/example/jonat/campfire/MyCampfireFragment.java
@@ -1,17 +1,41 @@
 package com.example.jonat.campfire;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-/**
- * Created by jonat on 25-Feb-2017.
- */
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MyCampfireFragment extends Fragment {
+
+    private ListView listView;
+    private String names[] = {
+            "Nicky",
+            "Paula",
+            "Rick",
+            "Mike"
+    };
+
+    private String desc[] = {
+            "Sick at UI",
+            "I like kernel programming",
+            "Code with Java Script!!!",
+            "Photoshop is my jam!"
+    };
+
+
+    private Integer imageid[] = {
+            R.drawable.person_icon,
+            R.drawable.person_icon,
+            R.drawable.person_icon,
+            R.drawable.person_icon
+    };
 
     @Nullable
     @Override
@@ -24,6 +48,26 @@ public class MyCampfireFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        MyCampfireList customList = new MyCampfireList(getActivity(), names, desc, imageid);
+
+        listView = (ListView) getView().findViewById(R.id.listOfTeam);
+        listView.setAdapter(customList);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                new AlertDialog.Builder(getActivity())
+                        .setTitle(names[i])
+                        .setMessage(desc[i])
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // continue with delete
+                            }
+                        })
+                        .setIcon(imageid[i])
+                        .show();
+            }
+        });
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("My Campfire");
     }
