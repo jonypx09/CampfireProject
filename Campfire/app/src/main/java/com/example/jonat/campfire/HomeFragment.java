4d41ff8fd38aa60e47dc10cdb6a15204d1cb74cb
Quinn Daneyko <quinn.daneyko@mail.utoronto.ca>
2017-03-01 00:24:02 -0500

    Very basic tinder swipe thing works

--- Campfire/app/src/main/java/com/example/jonat/campfire/HomeFragment.java
+++ Campfire/app/src/main/java/com/example/jonat/campfire/HomeFragment.java
@@ -1,17 +1,28 @@
 package com.example.jonat.campfire;
 
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.mindorks.placeholderview.SwipeDecor;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
+
 
 /**
  * Created by jonat on 25-Feb-2017.
  */
 
+//Used https://blog.mindorks.com/android-tinder-swipe-view-example-3eca9b0d4794#.lywnigtq7 quite
+//heavily.
 public class HomeFragment extends Fragment {
+
+    private SwipePlaceHolderView mSwipeView;
+    private Context mContext;
 
     @Nullable
     @Override
@@ -26,5 +37,40 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Home");
+        mSwipeView = (SwipePlaceHolderView) getActivity().findViewById(R.id.swipeView);
+        mContext = getActivity().getApplicationContext();
+
+        int bottomMargin = Utils.dpToPx(160);
+        Point windowSize = Utils.getDisplaySize(getActivity().getWindowManager());
+        mSwipeView.getBuilder()
+                .setDisplayViewCount(3)
+                .setHeightSwipeDistFactor(10)
+                .setWidthSwipeDistFactor(5)
+                .setSwipeDecor(new SwipeDecor()
+                        .setViewWidth(windowSize.x)
+                        .setViewHeight(windowSize.y - bottomMargin)
+                        .setViewGravity(Gravity.TOP)
+                        .setPaddingTop(20)
+                        .setRelativeScale(0.01f)
+                        .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
+                        .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
+
+        for (int i = 0; i < 6; i++) {
+            mSwipeView.addView(new TinderCard(mContext, mSwipeView));
+        }
+
+        getActivity().findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeView.doSwipe(false);
+            }
+        });
+
+        getActivity().findViewById(R.id.acceptBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeView.doSwipe(true);
+            }
+        });
     }
 }
