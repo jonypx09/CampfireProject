28d41e3809f35cb28c48be96c712999de5ee09de
Quinn Daneyko <quinn.daneyko@mail.utoronto.ca>
2017-03-04 20:05:53 -0500

    Changed swipeview to actually read from a list of Students and did a first look at a new layout.

--- Campfire/app/src/main/java/com/example/jonat/campfire/HomeFragment.java
+++ Campfire/app/src/main/java/com/example/jonat/campfire/HomeFragment.java
@@ -12,6 +12,13 @@ import android.view.ViewGroup;
 import com.mindorks.placeholderview.SwipeDecor;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
 
+import java.util.ArrayList;
+
+import backend.algorithms.Category;
+import backend.algorithms.Comparable;
+import backend.algorithms.Student;
+import backend.database.DatabaseAdapter;
+
 
 /**
  * Created by jonat on 25-Feb-2017.
@@ -23,6 +30,9 @@ public class HomeFragment extends Fragment {
 
     private SwipePlaceHolderView mSwipeView;
     private Context mContext;
+    private Student uStudent;
+    private ArrayList<Student> sampleStudents;
+    private ArrayList<Category> c1;
 
     @Nullable
     @Override
@@ -35,19 +45,37 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //you can set the title for your toolbar here for different fragments different titles
+
+        uStudent = ((MainActivity) getActivity()).getCurrentStudent();
+
+        c1 = new ArrayList<>();
+        c1.add(new Category("Hardware", 1));
+        c1.add(new Category("Kernel Programming", 2));
+        c1.add(new Category("Java", 1));
+        c1.add(new Category("Python Dev", 3));
+
+        sampleStudents = new ArrayList<>();
+        sampleStudents.add(new Student("Adam", "Capparelli", "adam@mail.com", "12345678", c1, null));
+        sampleStudents.add(new Student("Andrew", "Goupil", "andrew@mail.com", "12345678", c1, null));
+        sampleStudents.add(new Student("Fullchee", "Zhang", "fullchee@mail.com", "12345678", c1, null));
+        sampleStudents.add(new Student("Jonathan", "Pelastine", "jonathan@mail.com", "12345678", c1, null));
+        sampleStudents.add(new Student("Quinn", "Daneyko", "quinn@mail.com", "12345678", c1, null));
+        sampleStudents.add(new Student("Rod", "Mazloomi", "rod@mail.com", "12345678", c1, null));
+        sampleStudents.add(new Student("Vlad", "Chapurny", "vlad@mail.com", "12345678", c1, null));
+
         getActivity().setTitle("Home");
         mSwipeView = (SwipePlaceHolderView) getActivity().findViewById(R.id.swipeView);
         mContext = getActivity().getApplicationContext();
-
-        int bottomMargin = Utils.dpToPx(160);
+        //TODO: Probably not the best way of doing margin adjustments
+        int bottomMargin = Utils.dpToPx(180);
+        int sideMargin = Utils.dpToPx(32);
         Point windowSize = Utils.getDisplaySize(getActivity().getWindowManager());
         mSwipeView.getBuilder()
                 .setDisplayViewCount(3)
                 .setHeightSwipeDistFactor(10)
                 .setWidthSwipeDistFactor(5)
                 .setSwipeDecor(new SwipeDecor()
-                        .setViewWidth(windowSize.x)
+                        .setViewWidth(windowSize.x - sideMargin)
                         .setViewHeight(windowSize.y - bottomMargin)
                         .setViewGravity(Gravity.TOP)
                         .setPaddingTop(20)
@@ -55,8 +83,9 @@ public class HomeFragment extends Fragment {
                         .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
                         .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
 
-        for (int i = 0; i < 6; i++) {
-            mSwipeView.addView(new TinderCard(mContext, mSwipeView));
+        //TODO: Show applicable students, not sample.
+        for (Student s : sampleStudents) {
+            mSwipeView.addView(new TinderCard(getContext(), mSwipeView, s));
         }
 
         getActivity().findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
