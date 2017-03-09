7ae68e6843bc44a6d7246b504a9a880ddc09c13b
Quinn Daneyko <quinn.daneyko@mail.utoronto.ca>
2017-03-09 01:19:24 -0500

    Changed lots

--- Campfire/app/src/main/java/com/example/jonat/campfire/HomeFragment.java
+++ Campfire/app/src/main/java/com/example/jonat/campfire/HomeFragment.java
@@ -9,13 +9,15 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import com.mindorks.placeholderview.SwipeDecor;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
 
 import java.util.ArrayList;
 
-import backend.algorithms.Category;
 import backend.algorithms.Comparable;
+import backend.algorithms.Course;
 import backend.algorithms.HobbiesCriteria;
 import backend.algorithms.ProgrammingLanguagesCriteria;
 import backend.algorithms.Student;
@@ -30,19 +32,25 @@ import backend.database.DatabaseAdapter;
 //heavily.
 public class HomeFragment extends Fragment {
 
+    private String[] newStudentID;
     private SwipePlaceHolderView mSwipeView;
     private Context mContext;
     private Student uStudent;
-    private ArrayList<Student> sampleStudents;
+    private ArrayList<Student> allStudents;
+    private ArrayList<Student> otherStudents301;
+    private ArrayList<Student> students301;
     private ArrayList<String> c1;
     private ArrayList<String> c2;
     ArrayList<Comparable> crit;
+    DatabaseAdapter db;
+    Course csc301;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
+        newStudentID = getArguments().getStringArray("identity");
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
@@ -51,38 +59,15 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        db = new DatabaseAdapter(getContext());
+        allStudents = db.getAllStudents();
+        csc301 = db.getCourse("csc301h1");
+        students301 = db.getStudentsInCourse("csc301h1");
+
         uStudent = ((MainActivity) getActivity()).getCurrentStudent();
+        //TODO: Does not work, includes current student.
+        otherStudents301 = uStudent.getallOtherCourseStudents(csc301);
 
-        c1 = new ArrayList<>();
-        c1.add("Java");
-        c1.add("HTML");
-        c1.add("CSS");
-        c1.add("JavaScript");
-
-        c2 = new ArrayList<>();
-        c2.add("Running");
-        c2.add("Video Games");
-        c2.add("Reading");
-        c2.add("Watching Sports");
-
-        ProgrammingLanguagesCriteria languages = new ProgrammingLanguagesCriteria(c1);
-        HobbiesCriteria hobbies = new HobbiesCriteria(c2);
-
-        crit = new ArrayList<>();
-        crit.add(languages);
-        crit.add(hobbies);
-
-        sampleStudents = new ArrayList<>();
-        sampleStudents.add(new Student("Adam", "Capparelli", "adam@mail.com", "12345678", crit));
-        sampleStudents.add(new Student("Andrew", "Goupil", "andrew@mail.com", "12345678", crit));
-        sampleStudents.add(new Student("Fullchee", "Zhang", "fullchee@mail.com", "12345678", crit));
-        sampleStudents.add(new Student("Jonathan", "Pelastine", "jonathan@mail.com", "12345678", crit));
-        sampleStudents.add(new Student("Quinn", "Daneyko", "quinn@mail.com", "12345678", crit));
-        sampleStudents.add(new Student("Rod", "Mazloomi", "rod@mail.com", "12345678", crit));
-        sampleStudents.add(new Student("Vlad", "Chapurny", "vlad@mail.com", "12345678", crit));
-        for (Student s : sampleStudents) {
-            s.setDescription("Sample Description");
-        }
         getActivity().setTitle("Home");
         mSwipeView = (SwipePlaceHolderView) getActivity().findViewById(R.id.swipeView);
         mContext = getActivity().getApplicationContext();
@@ -103,11 +88,20 @@ public class HomeFragment extends Fragment {
                         .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
                         .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
 
-        //TODO: Show applicable students, not sample.
-        for (Student s : sampleStudents) {
-            mSwipeView.addView(new TinderCard(getContext(), mSwipeView, s));
-        }
+        // Change swipeOn to:
+        //     - allStudents for every student in db.
+        //     - otherStudents301 for all other students in csc301h1. (Currently unavailable)
+        //     - students301 for all students in csc301h1.
 
+        ArrayList<Student> swipeOn = otherStudents301;
+        if (swipeOn.isEmpty()) {
+            Toast.makeText(getContext(), "No available matches.", Toast.LENGTH_LONG).show();
+        }
+        else {
+            for (Student s : swipeOn) {
+                mSwipeView.addView(new TinderCard(getContext(), mSwipeView, s));
+            }
+        }
         getActivity().findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
