81a389a30da1d8579c9a8680b029f49a3d616685
jonypx19 <jonathan.pelastine@mail.utoronto.ca>
2017-03-19 00:43:28 -0400

    Removed unused imports

--- Campfire/app/src/main/java/com/example/jonat/campfire/SplashActivity.java
+++ Campfire/app/src/main/java/com/example/jonat/campfire/SplashActivity.java
@@ -1,10 +1,8 @@
 package com.example.jonat.campfire;
 
 import android.content.Intent;
-import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
